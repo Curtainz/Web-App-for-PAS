c4cfaf9e703b0cb9e5fae91dbc0c61c0840ed659
Zhang Cang <zielene@OUTLOOK.COM>
2025-07-15 14:07:02 +0800

    Update docker_run.bat

--- xray-docker/docker_run.bat
+++ xray-docker/docker_run.bat
@@ -1,5 +1,6 @@
 docker run --name web-app-for-pas ^
   -p 5000:5000 ^
-  -v D:\GitHub\PAS models\F_M1_90:/app/models/F_M1_90 ^
+  -v D:\GitHub\PAS-models\F_M1_90:/app/models/F_M1_90 ^
   -v %cd%\uploads:/app/uploads ^
   xray-docker:latest
+pause
