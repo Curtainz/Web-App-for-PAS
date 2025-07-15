c9a11562d51c58ff4582a38ba345cb9f55f6a1c2
Zhang Cang <zielene@OUTLOOK.COM>
2025-07-15 14:20:25 +0800

    Uploaded Model

--- xray-docker/docker_run.bat
+++ xray-docker/docker_run.bat
@@ -1,6 +1,6 @@
 docker run --name web-app-for-pas ^
   -p 5000:5000 ^
-  -v D:\GitHub\PAS-models\F_M1_90:/app/models/F_M1_90 ^
-  -v %cd%\uploads:/app/uploads ^
+  -v "D:/GitHub/PAS-models/F_M1_90":/models/F_M1_90 ^
+  -v "D:/GitHub/Web-App-for-PAS/xray-docker/uploads":/uploads ^
   xray-docker:latest
 pause
